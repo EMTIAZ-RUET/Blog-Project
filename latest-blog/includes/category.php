3297b7741f3d37a2cfdf93ad53d7c30c349d1a39
EMTIAZ UDDIN AHMED <56144461+EMTIAZ-RUET@users.noreply.github.com>
2022-06-22 17:31:36 +0600

    Add files via upload

---
+++ latest-blog/includes/category.php
@@ -0,0 +1,19 @@
+<div class="col-lg-12">
+                  <div class="sidebar-item categories">
+                    <div class="sidebar-heading">
+                      <h2>Categories</h2>
+                    </div>
+                    <div class="content">
+                      <ul>
+                     
+                      <?php while($category=mysqli_fetch_assoc($getcat)){?>
+                      <li>
+                     <a  href="index.php">
+                     <?php    echo $category['cat_name'];?>
+                      </a>
+                      </li> 
+                     <?php } ?>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
